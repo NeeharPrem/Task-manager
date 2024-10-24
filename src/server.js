dedcdc1bca71634236fa52db5eb19435777c23c0
NeeharPrem <neeharprem94@gmail.com>
2024-10-24 14:23:23 +0530

    fix: fixed module import issue

--- src/server.js
+++ src/server.js
@@ -3,11 +3,16 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 dotenv.config();
 
+const PORT = process.env.PORT || 3000;
+
 const app= express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URI)
-.then(() => logger.info('MongoDB connected'))
-.catch(err => logger.error('MongoDB connection error:', err));
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
