19458dbd4f5771988470414b34dbfa89456588dc
NeeharPrem <neeharprem94@gmail.com>
2024-10-25 05:42:55 +0530

    Cors Implimented

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ import rateLimit from 'express-rate-limit';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import { socketIOSetup } from './services/socket.js';
+import cors from 'cors';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -20,6 +21,12 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'))
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL || '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
